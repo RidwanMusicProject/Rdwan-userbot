5ad740742c2ec0c93670669d660a85fb66cafdd8
pocong <81405011+poocong@users.noreply.github.com>
2022-03-13 12:02:06 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -160,7 +160,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https:https://github.com/RidwanMusicProject/Rdwan-userbot.git"
+    "UPSTREAM_REPO_URL", "https://github.com/RidwanMusicProject/Rdwan-userbot.git"
 )
 
 # Custom name sticker pack
